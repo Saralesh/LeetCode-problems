b0f4bbd609251c828f514a274ae182ee1076cc63
Saralesh <148570224+Saralesh@users.noreply.github.com>
2024-09-14 09:19:05 +0530

    Time: 11 ms (32.95%), Space: 40.5 MB (97.57%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,48 +1,42 @@
 class Solution {
-    public void solveSudoku(char[][] board) {
-        solve(board);
-        }
-        public static boolean solve(char[][] board){
-        for(int i = 0; i < 9;i++){
-            for(int j = 0; j < 9;j++){
+    public static boolean solver(char[][] board){
+        for(int i = 0;i < 9;i++){
+            for(int j = 0;j < 9;j++){
                 if(board[i][j] == '.'){
-                    for(char ch = '1';ch <= '9';ch++){
+                    for(char ch = '1'; ch <= '9' ; ch++){
                         if(is_valid(board,i,j,ch)){
                             board[i][j] = ch;
-                            if(solve(board)){
+                            if(solver(board)){
                                 return true;
                             }
                             else{
                                 board[i][j] = '.';
                             }
                         }
-
-
-
-
                     }
                     return false;
-                    
-                    }
                 }
             }
-            return true;
         }
-        public static boolean is_valid(char[][] board,int row,int col,char ch){
-            for(int i = 0;i < 9;i++){
-                if(board[row][i] == ch){
-                    return false;
-                }
-                if(board[i][col] == ch){
-                    return false;
-                }
-                if(board[(row/3)*3+i/3][(col/3)*3+i%3]==ch){
-                    return false;
-                }
-
-
-                }
-                return true;
+        return true;
+    }
+    public static boolean is_valid(char[][] board,int row, int col,char ch){
+        for(int i = 0; i < 9;i++){
+            if(board[row][i] == ch){
+                return false;
             }
+            if(board[i][col] == ch){
+                return false;
+            }
+            if(board[(row / 3) * 3 + i / 3][(col / 3) * 3 + i % 3] == ch){
+                return false;
+            }
+
         }
-        
+        return true;
+    }
+
+    public void solveSudoku(char[][] board) {
+        solver(board);
+    }
+}
