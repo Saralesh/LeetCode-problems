7f3d472d24914ca146a5723dbbc02274d0a14e7f
Saralesh <148570224+Saralesh@users.noreply.github.com>
2024-09-28 11:08:41 +0530

    Time: 2 ms (89.47%), Space: 44.5 MB (95.16%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,47 +1,58 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> res = new ArrayList<>();
-        backtrack(res, new ArrayList<>(), 0, n);
-        return res;
-    }
+        boolean[] rowf = new boolean[n];
+        boolean[] colf = new boolean[n];
+        boolean[] trblf = new boolean[2*n-1];
+        boolean[] tlbrf = new boolean[2*n-1];
+        boolean[][] board = new boolean[n][n];
 
-    private void backtrack(List<List<String>> res, List<Integer> pos, int row, int n) {
-        if (row == n) {
-            res.add(formString(pos));
-        }
-        for (int col = 0; col < n; ++col) {
-            if (isValid(pos, row, col)) {
-                pos.add(col);
-                backtrack(res, pos, row + 1, n);
-                pos.remove(pos.size() - 1);
-            }
-        }
+        List<List<String>> ans = new ArrayList<>();
+        solve(rowf,colf,trblf,tlbrf,board,ans,n,0);
+        return ans;
     }
-
-    private boolean isValid(List<Integer> pos, int row, int col) {
-        for (int rowTaken = 0; rowTaken < pos.size(); ++rowTaken) {
-            int colTaken = pos.get(rowTaken);
-            if (col == colTaken || Math.abs(row - rowTaken) == Math.abs(col - colTaken)) {
-                return false;
-            }
-        }
-        return true;
-    }
-    
-    private List<String> formString(List<Integer> pos) {
-        List<String> res = new ArrayList<>();
-        int n = pos.size();
-        for (int col : pos) {
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < n; ++j) {
-                if (j == col) {
-                    sb.append("Q");
-                } else {
-                    sb.append(".");
+    public void solve(boolean[] rowf, boolean[] colf, boolean[] trblf, 
+    boolean[] tlbrf, boolean[][] board, List<List<String>> ans, int n,int row){
+        if(row == n){
+            List<String> temp = new ArrayList<>();
+            for(int r = 0;r<n;r++){
+                StringBuilder sb = new StringBuilder();
+                for(int c = 0;c<n;c++){
+                    if(board[r][c]){
+                        sb.append("Q");
+                    }else{
+                        sb.append(".");
+                    }
                 }
+                temp.add(sb.toString());
             }
-            res.add(sb.toString());
+            ans.add(temp);
+            return;
         }
-        return res;
+
+        for(int col = 0; col < n;col++){
+            if(isvalid(rowf,colf,trblf,tlbrf,board,n,row,col)){
+            rowf[row] = true;
+            colf[col] = true;
+            trblf[row+col] =true;
+            tlbrf[n-1-(row-col)] = true;
+            board[row][col] = true;
+            solve(rowf,colf,trblf,tlbrf,board,ans,n,row+1);
+            rowf[row] = false;
+            colf[col] = false;
+            trblf[row+col] =false;
+            tlbrf[n-1-(row-col)] = false;
+            board[row][col] = false;
+            }
+        }
+
+    }
+
+    boolean isvalid(boolean[] rowf, boolean[] colf, boolean[] trblf, 
+    boolean[] tlbrf, boolean[][] board,int n,int row,int col){
+        if(rowf[row] || colf[col] || trblf[row+col] || tlbrf[n - 1-(row-col)] ||
+        board[row][col]){
+            return false;
+        } 
+        return true;
     }
 }
