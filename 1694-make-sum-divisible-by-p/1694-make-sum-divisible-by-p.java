2bf63a64137a4d0c6ac7956fdd9860bd58f68c4c
Saralesh <148570224+Saralesh@users.noreply.github.com>
2024-10-03 18:46:09 +0530

    Time: 24 ms (81.69%), Space: 56.7 MB (28.17%) - LeetHub

--- 1694-make-sum-divisible-by-p/1694-make-sum-divisible-by-p.java
+++ 1694-make-sum-divisible-by-p/1694-make-sum-divisible-by-p.java
@@ -3,23 +3,28 @@ class Solution {
         int l = nums.length;
         int sum = 0;
         for(int num : nums){
-            sum = (sum + num) % p;
+            sum = (sum + num) % p; 
         }
         int target = sum % p;
         if(target == 0) return 0;
-        HashMap<Integer,Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
         map.put(0,-1);
-        int currentsum  = 0;
         int minlen = l;
-        for(int i =0; i < l;++i){
+        int currentsum = 0;
+        for(int i = 0;i < l;i++){
             currentsum = (currentsum + nums[i]) % p;
             int needed = (currentsum - target + p) % p;
             if(map.containsKey(needed)){
-                minlen = Math.min(minlen, i - map.get(needed));
+                minlen = Math.min(minlen,i - map.get(needed));
             }
             map.put(currentsum,i);
         }
-        return minlen == l ? -1 : minlen;
-
+        if(minlen == l){
+        return -1;
     }
+    else{
+        return minlen;
+    }
+    } 
+
 }
