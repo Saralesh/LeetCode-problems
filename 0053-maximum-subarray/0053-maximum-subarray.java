b1be2b4ebda8350f609eb60b4dbfc77e037189c4
Saralesh <148570224+Saralesh@users.noreply.github.com>
2024-12-04 18:49:41 +0530

    Time: 3 ms (6.36%), Space: 52.9 MB (99.9%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -4,8 +4,8 @@ class Solution {
         int sum = 0;
         for(int i = 0;i < nums.length;i++){
             sum += nums[i];
-           if(sum > maxi) maxi = sum;
-           if(sum < 0) sum = 0;           
+            maxi = sum > maxi ? sum : maxi;
+            sum = sum < 0 ? 0 : sum;       
         }
         System.gc();
         return maxi;
