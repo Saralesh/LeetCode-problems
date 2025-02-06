87003c2d22ad6b5c50fde19b6f7b7989e5a48527
Saralesh <148570224+Saralesh@users.noreply.github.com>
2025-02-06 11:49:09 +0530

    Time: 0 ms (100%), Space: 41.4 MB (94.81%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
@@ -9,7 +9,9 @@ class Solution {
         }
         if(arr.size() == 0) return true;
         if(arr.size() != 2) return false;
-        if((s1.charAt(arr.get(0)) == s2.charAt(arr.get(1))) && (s1.charAt(arr.get(1)) == s2.charAt(arr.get(0)))){
+        int f = arr.get(0);
+        int l = arr.get(1);
+        if(((s1.charAt(f)) == s2.charAt(l)) && (s1.charAt(l) == s2.charAt(f))){
             return true;
         }
         return false;
