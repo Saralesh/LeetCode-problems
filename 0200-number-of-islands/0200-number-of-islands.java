bed60dbd0a128a7a898474e499da6727b5eca737
Saralesh <148570224+Saralesh@users.noreply.github.com>
2025-03-26 11:03:18 +0530

    Time: 3 ms (86.31%), Space: 49.7 MB (52.24%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,30 +1,28 @@
 class Solution {
-    public static void dfs(char[][] grid,int x, int y, int r, int c){
-        if(x < 0 || x >= r || y < 0 || y >= c || grid[x][y] == '0' || grid[x][y] == '2'){
-            return;
-        }
-        grid[x][y] = '2';
-        dfs(grid,x+1,y,r,c);
-        dfs(grid,x,y+1,r,c);
-        dfs(grid,x-1,y,r,c);
-        dfs(grid,x,y-1,r,c);
-    }
+    static int r;
+    static int c;
     public int numIslands(char[][] grid) {
-        
+        r = grid.length;
+        c = grid[0].length;
         int count = 0;
-        int r = grid.length;
-        int c = grid[0].length;
-        if(r == 0) return 0;
-       
-        for(int i = 0; i < r; i++){
-            for(int j = 0; j < c; j++){
+        for(int i = 0;i < r;i++){
+            for(int j = 0;j < c;j++){
                 if(grid[i][j] == '1'){
-                dfs(grid,i,j,r,c);
                 count++;
+                solve(grid,i,j,count);
                 }
             }
         }
         return count;
-        
+    }
+    public static void solve(char[][] grid,int i,int j,int count){
+        if(i >= r || i < 0 || j >= c || j < 0 || grid[i][j] == '0'){
+            return;
+        }
+        grid[i][j] = '0';
+        solve(grid,i,j-1,count);
+        solve(grid,i,j+1,count);
+        solve(grid,i-1,j,count);
+        solve(grid,i+1,j,count);
     }
 }
