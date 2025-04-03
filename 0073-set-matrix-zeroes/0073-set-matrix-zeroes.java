c30487ce677674d1309cd350e5e8770c5117f68c
Saralesh <148570224+Saralesh@users.noreply.github.com>
2025-04-03 12:14:54 +0530

    Time: 1 ms (75.11%), Space: 45.8 MB (46.07%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -4,23 +4,20 @@ class Solution {
         int c = matrix[0].length;
         int[][] result = new int[r][c];
 
-        // Step 1: Copy original matrix into result
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
                 result[i][j] = matrix[i][j];
             }
         }
 
-        // Step 2: Find zeros and mark corresponding rows and columns
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
                 if (matrix[i][j] == 0) {
-                    solve(result, i, j); // Modify result matrix
+                    solve(result, i, j);
                 }
             }
         }
 
-        // Step 3: Copy result back to matrix (modifies in-place)
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
                 matrix[i][j] = result[i][j];
@@ -32,11 +29,9 @@ class Solution {
         int r = result.length;
         int c = result[0].length;
 
-        // Set entire column to 0
         for (int i = 0; i < r; i++) {
             result[i][col] = 0;
         }
-        // Set entire row to 0
         for (int j = 0; j < c; j++) {
             result[row][j] = 0;
         }
