7d41b8b69a23b34fe40cc94b829be83c31b8c03b
Saralesh <148570224+Saralesh@users.noreply.github.com>
2025-09-19 13:53:21 +0530

    Time: 1 ms (45.66%), Space: 14.7 MB (56.69%) - LeetHub

--- 0089-gray-code/0089-gray-code.cpp
+++ 0089-gray-code/0089-gray-code.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> grayCode(int n) {
         vector<int> result;
-        int total = 1 << n;
+        int total = (int)pow(2,n);
         for(int i = 0;i < total;i++){
             result.push_back(i ^ (i >> 1));
         }
